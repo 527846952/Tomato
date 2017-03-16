e14a0c967404b45c1e94803f6880645b2fdf8b3d
Zero <527846952@qq.com>
2017-03-17 01:03:15 +0800

    构建原始的业务逻辑

---
+++ Tomato/Tomato/TomatoPlant.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomato
+{
+    enum TOMATO_PLANT_STATE
+    {
+        Growing,
+        Pause,
+        Giveup,
+        Reaped,
+    }
+
+    class TomatoPlant
+    {
+        private readonly int TomatoPlantLifeSeconds = 1500;
+        private TomatoSeed seed;
+        private int remainLifeSeconds;
+        private TOMATO_PLANT_STATE state;
+        public TOMATO_PLANT_STATE State
+        {
+            get
+            {
+                return state;
+            }
+        }
+
+        public event Action<double> OnRateChange;
+        public event Action OnReapFruit;
+
+        public TomatoPlant(TomatoSeed oriSeed)
+        {
+            seed = oriSeed;
+            remainLifeSeconds = TomatoPlantLifeSeconds;
+        }
+
+        public void TimeLoseSecond()
+        {
+            if (state != TOMATO_PLANT_STATE.Growing)
+            {
+                return;
+            }
+            remainLifeSeconds = Math.Max(0, remainLifeSeconds - 1);
+            OnRateChange(1 - remainLifeSeconds / (double)TomatoPlantLifeSeconds);
+            if (remainLifeSeconds <= 0)
+            {
+                state = TOMATO_PLANT_STATE.Reaped;
+                OnReapFruit?.Invoke();
+            }
+        }
+
+        public void Giveup(string giveupTip)
+        {
+            if (state != TOMATO_PLANT_STATE.Growing ||
+                state != TOMATO_PLANT_STATE.Pause)
+            {
+                throw new Exception("TomatoPlant giveup fail, state is " + state);
+            }
+            state = TOMATO_PLANT_STATE.Giveup;
+        }
+
+        public void Pause(string pauseTip)
+        {
+            if (state != TOMATO_PLANT_STATE.Growing)
+            {
+                throw new Exception("TomatoPlant pause fail, state is " + state);
+            }
+            state = TOMATO_PLANT_STATE.Pause;
+        }
+
+        public void Replay()
+        {
+            if (state != TOMATO_PLANT_STATE.Pause)
+            {
+                throw new Exception("TomatoPlant replay fail, state is " + state);
+            }
+            state = TOMATO_PLANT_STATE.Growing;
+        }
+    }
+}
---
+++ Tomato/Tomato/TomatoSeed.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomato
+{
+    enum TOMATO_PRI
+    {
+        S,
+        A,
+        B,
+        C,
+        D,
+    }
+    enum TOMATO_SEED_STATE
+    {
+        Ready,
+        Sowed
+    }
+
+    class TomatoSeed
+    {
+        private string title;
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+        private string detail;
+        public string Detail
+        {
+            get
+            {
+                return detail;
+            }
+
+            set
+            {
+                detail = value;
+            }
+        }
+        private int expectTomatoCount;
+        public int ExpectTomatoCount
+        {
+            get
+            {
+                return expectTomatoCount;
+            }
+
+            set
+            {
+                expectTomatoCount = value;
+            }
+        }
+        private TOMATO_SEED_STATE state;
+        public TOMATO_SEED_STATE State
+        {
+            get
+            {
+                return state;
+            }
+        }
+
+        public TomatoSeed()
+        {
+            state = TOMATO_SEED_STATE.Ready;
+        }
+
+        public TomatoPlant Sow()
+        {
+            if (state != TOMATO_SEED_STATE.Ready)
+            {
+                throw new Exception("TomatoSeed sow fail, state is " + state);
+            }
+            state = TOMATO_SEED_STATE.Sowed;
+            return new TomatoPlant(this);
+        }
+    }
+}
